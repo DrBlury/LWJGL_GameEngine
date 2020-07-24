98e34d50046165431432ece888fa96ab07f89afd
Julian Bensch <Bensch.Julian@gmail.com>
2020-07-24 10:40:41 +0200

    Introduced new Method for mesh creation

--- src/main/java/org/drblury/gameengine/game/DummyGame.java
+++ src/main/java/org/drblury/gameengine/game/DummyGame.java
@@ -9,11 +9,8 @@ import static org.lwjgl.glfw.GLFW.*;
 public class DummyGame implements IGameLogic {
 
     private int direction = 0;
-
     private float color = 0.0f;
-
     private final Renderer renderer;
-
     private Mesh mesh;
 
     public DummyGame() {
@@ -23,22 +20,7 @@ public class DummyGame implements IGameLogic {
     @Override
     public void init(Window window) throws Exception {
         renderer.init(window);
-        float[] positions = new float[]{
-                -0.5f,  0.5f, -1.05f,
-                -0.5f, -0.5f, -1.05f,
-                0.5f, -0.5f, -1.05f,
-                0.5f,  0.5f, -1.05f,
-        };
-        float[] colours = new float[]{
-                0.5f, 0.0f, 0.0f,
-                0.0f, 0.5f, 0.0f,
-                0.0f, 0.0f, 0.5f,
-                0.0f, 0.5f, 0.5f,
-        };
-        int[] indices = new int[]{
-                0, 1, 3, 3, 1, 2,
-        };
-        mesh = new Mesh(positions, colours, indices);
+        createMesh();
     }
 
     @Override
@@ -73,5 +55,26 @@ public class DummyGame implements IGameLogic {
         renderer.cleanup();
         mesh.cleanUp();
     }
+
+    private void createMesh() {
+        float[] positions = new float[]{
+                -0.5f,  0.5f, -1.05f,
+                -0.5f, -0.5f, -1.05f,
+                0.5f, -0.5f, -1.05f,
+                0.5f,  0.5f, -1.05f,
+        };
+        float[] colours = new float[]{
+                0.5f, 0.0f, 0.0f,
+                0.0f, 0.5f, 0.0f,
+                0.0f, 0.0f, 0.5f,
+                0.0f, 0.5f, 0.5f,
+        };
+        int[] indices = new int[]{
+                0, 1, 3, 3, 1, 2,
+        };
+        mesh = new Mesh(positions, colours, indices);
+    }
+
+
 
 }
