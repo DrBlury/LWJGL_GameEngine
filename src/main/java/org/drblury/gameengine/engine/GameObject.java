892962b604c7c6a38803df516f4a8b1e6057d8a0
Julian Bensch <Bensch.Julian@gmail.com>
2020-07-28 23:06:54 +0200

    bugfix

--- src/main/java/org/drblury/gameengine/engine/GameObject.java
+++ src/main/java/org/drblury/gameengine/engine/GameObject.java
@@ -3,30 +3,31 @@ package org.drblury.gameengine.engine;
 import org.drblury.gameengine.engine.graph.Mesh;
 import org.joml.Vector3f;
 
-public class GameItem {
-    private final Mesh mesh;
-    private Vector3f position;
+public class GameObject {
+    private Mesh mesh;
+    private final Vector3f position;
     private float scale;
-    private Vector3f rotation;
+    private final Vector3f rotation;
 
-    public Mesh getMesh() {
-        return mesh;
+    public GameObject() {
+        position = new Vector3f();
+        scale = 1;
+        rotation = new Vector3f();
+    }
+
+    public GameObject(Mesh mesh) {
+        this();
+        this.mesh = mesh;
+    }
+
+    public Vector3f getPosition() {
+        return position;
     }
 
     public void setPosition(float x, float y, float z) {
         this.position.x = x;
         this.position.y = y;
         this.position.z = z;
-    }
-
-    public void setRotation(float x, float y, float z) {
-        this.rotation.x = x;
-        this.rotation.y = y;
-        this.rotation.z = z;
-    }
-
-    public Vector3f getPosition() {
-        return position;
     }
 
     public float getScale() {
@@ -41,10 +42,17 @@ public class GameItem {
         return rotation;
     }
 
-    public GameItem(Mesh mesh) {
+    public void setRotation(float x, float y, float z) {
+        this.rotation.x = x;
+        this.rotation.y = y;
+        this.rotation.z = z;
+    }
+
+    public Mesh getMesh() {
+        return mesh;
+    }
+
+    public void setMesh(Mesh mesh) {
         this.mesh = mesh;
-        this.position = new Vector3f(0, 0, 0);
-        this.scale = 1;
-        this.rotation = new Vector3f(0, 0, 0);;
     }
 }
